58c77f24c094b6ddd5b6954de103f541a77895c0
realizelenaa <elenarealisanx@gmail.com>
2023-11-09 18:45:05 +0800

    First commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    
+    public static void main(String[] args){
+        
+        Parent myParent = new Parent("Joy");
+        
+        myParent.cook();
+        myParent.clean();
+    }
+    
+}
---
+++ src/Parent.java
@@ -0,0 +1,15 @@
+public class Parent {
+    String name;
+    
+    Parent(String name){
+        this.name = name;
+    }
+    
+    void cook(){
+        System.out.println(name + " is cooking");
+    }
+    
+    void clean(){
+        System.out.println(name + " is cleaning");
+    }
+}
